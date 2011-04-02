737d92b2aa0cf1a7cff0fadfd79f99a9dd0faab9
Tomislav Capan <tomislav@netko.hr>
2011-04-02 22:18:57 +0200

    Prettify code support; body font tweak adds 'Liberation Serif' for linux as default (JoliOS serif font)

---
+++ Scripts/script.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function(){
+	styleCode();
+	
+	function styleCode() {
+		var shouldPrettify = false;
+		$("pre code").parent().each(function() {
+			if (!$(this).hasClass("prettyprint")) {
+				$(this).addClass("prettyprint");
+				shouldPrettify = true;
+			}
+		});
+		if (shouldPrettify) { prettyPrint(); } 
+	}
+});
